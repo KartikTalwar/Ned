3a9147af1e8c5cdcc64acae014d0d1c0d438fed7
KartikTalwar <hi@kartikt.com>
2012-04-25 22:53:11 -0700

    Converted calculator

--- plugins/calculator.js
+++ plugins/calculator.js
@@ -1,123 +1,75 @@
+var plugin = {
+              name        : 'calculator',
+              trigger     : ['compute', 'calculate', 'calc', 'math', 'convert'],
+              enabled     : 'true',
+              fuzzy       : 'true',
+              description : 'Does your math',
+              usage       : 'ned calc calc pi*5+(i^2)'
+             };
 
-var trigger = ['compute', 'calculate', 'calc', 'math', 'convert'];
-var help    = [{
-               usage       : 'calc OR calculate', 
-               description : 'Will crunch some numbers [@ned calc pi*5+(i^2)]'
-              }];
-module.exports.help = help
+module.exports.plugin = plugin;
 
-
-
-module.exports.load = function(bot) 
+module.exports[plugin.name] = function(get)
 {
-    var assembleInput = "("+trigger.join("|")+")";    
-    var callerRegEx   = new RegExp(Util.NedCaller.source + Util.NedName.source   + " " + assembleInput + "(.*)$", "i");
-    var pmCallerRegEx = new RegExp(Util.NedCaller.source + Util.NedPMName.source + ""  + assembleInput + "(.*)$", "i");
-
-    bot.onMessage(callerRegEx, onMessage);
-    bot.onPrivateMessage(pmCallerRegEx, onMessage);
-};
-
-
-var onMessage = function(channel, frm, msg, x) 
-{
-    var self             = this;
-    var isPrivateMessage = (arguments.length == 3) ? true : false;
-    var from             = isPrivateMessage ? '' : frm;
-    var tempMessage      = isPrivateMessage ? frm : msg;
-    var roomName         = channel.split('@')[0];
-    var isSingleWord     = (tempMessage.indexOf(" ") == -1) ? true : false;
-
-    var caller           = /^([\@\!\#\$\~\%\/\\/]*)?/;
-    var ned              = (isPrivateMessage && isSingleWord) ? 
-                           /([Nn][Ee][Dd][Dd]?([Dd]?[Aa][Rr][Dd])?(( *[^A-Za-z0-9]* *))?)?$/ :
-                           /([Nn][Ee][Dd][Dd]?([Dd]?[Aa][Rr][Dd])?(( *[^A-Za-z0-9]* *))? ?)?/; 
-
-    var assembleInput    = "("+trigger.join("|")+")";
-    var regEx            = new RegExp(caller.source + ned.source + assembleInput, "i");
-    var message          = tempMessage.replace(regEx, '');
-    var isEmpty          = (message.split(' ').join('').length == 0 || message.length == 0) ? true : false;
-    var isPrivate        = (isPrivateMessage) ? 1 : 0;
-    var message          = Util.clarify(message.split("+").join("%2B"));
-
-    if(isEmpty)
+    if(get.isEmpty)
     {
-        self.message(channel, Util.errorMessage() + "Please check your input");
+        sendMessage(Util.errorMessage() + "Please check your input");
     }
     else
     {
-        var input = Util.padd(message);
+        var input = Util.padd(get.message);print(get);
 
-
-        if(input.length > 0)
+        var httpRequestParams = 
         {
+            host: "www.google.com",
+            port: 80,
+            path: "/ig/calculator?q=" + input
+        };
 
-            // Actual function begins here
+        http.get(httpRequestParams, function(res) 
+        {
+            var data = '';
 
-
-            var httpRequestParams = 
+            res.on('data', function(chunk) 
             {
-                host: "www.google.com",
-                port: 80,
-                path: "/ig/calculator?q=" + input
-            };
-
-            http.get(httpRequestParams, function(res) 
+                data += chunk;
+            });
+            
+            res.on('end', function(chunk) 
             {
-                var data = '';
-                res.on('data', function(chunk) {
-                    data += chunk;
-                });
-                
-                res.on('end', function(chunk) 
+                var computed = Util.cut('rhs: "', '"', data).toString("utf8");
+                var bad      = ['/', 'x26', 'x3c', 'x3e', '/sup', 'sup', '#215;', '�'];
+                var good     = ['',  '',    '',    '',    '',     '^',   '*',     '' ];
+                    computed = Util.str_replace(bad, good, computed);
+                                    
+
+                if(Util.it_has(input, '/0') || Util.it_has(input, '/+0'))
                 {
-                    computed = Util.cut('rhs: "', '"', data).toString("utf8");
-                    computed = computed.replace(/\\/g, "");
-                    computed = computed.replace(/x26/g, '');
-                    computed = computed.replace(/x3c/g, '');
-                    computed = computed.replace(/x3e/g, '');
-                    computed = computed.replace(/\/sup/g, '');
-                    computed = computed.replace(/sup/g, '^');
-                    computed = computed.replace(/#215;/g, '*');
-                    computed = computed.replace(/�/g, '');
-                                        
-
-                    if(input.indexOf('/0') != -1 || input.indexOf('/+0') != -1)
+                    if(Util.it_has(input, '/0.') && Util.it_has(input, '/+0.'))
                     {
-                        if(input.indexOf('/0.') == -1 && input.indexOf('/+0.') == -1)
-                        {
-                            self.message(channel, "Only Chuck Norris can divide by zero");
-                        }
-                        else
-                        {
-                            self.message(channel, computed.split('(')[0]);
-                        }
+                        sendMessage("Division by zero error. Only Chuck Norris can divide by zero");
                     }
                     else
                     {
-                        try
-                        {
-                           self.message(channel, computed.split('(')[0]);
-                        }
-                        catch(err)
-                        {
-                            self.message(channel, "(facepalm) Error");
-                        }
+                        sendMessage(computed.split('(')[0]);
                     }
-                });
+                }
+                else
+                {
+                    try
+                    {
+                       sendMessage(computed.split('(')[0]);
+                    }
+                    catch(err)
+                    {
+                        sendMessage("(facepalm) Error");
+                    }
+                }
             });
-            
-        // actual function ends here
+        });
 
-        }
-        else
-        {
-            self.message(channel, Util.errorMessage() + "Please check your input");
-        }
     }
 
 
     return true;
-};
-
-
+}
