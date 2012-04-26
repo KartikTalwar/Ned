284af17d85563ad99843898c4f378a49bf625508
KartikTalwar <hi@kartikt.com>
2012-04-26 00:02:08 -0700

    Converted youtube

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -1,100 +1,55 @@
+var plugin = {
+              name        : 'youtube',
+              trigger     : ['yt', 'youtube', 'utube', 'watch', 'video of'],
+              enabled     : 'true',
+              fuzzy       : 'false',
+              description : 'Returns the first youtube video',
+              usage       : 'ned yt nyan cat'
+             };
 
-var trigger = ['yt', 'youtube', 'utube', 'watch', 'video of'];
-var help    = [{
-               usage       : 'yt OR youtube', 
-               description : 'This will return the requested video [@ned yt (xyz)]'
-              }];
-module.exports.help = help
+module.exports.plugin = plugin;
 
-
-
-module.exports.load = function(bot) 
+module.exports[plugin.name] = function(get)
 {
-    var assembleInput = "("+trigger.join("|")+")";    
-    var callerRegEx   = new RegExp(Util.NedCaller.source + Util.NedName.source   + " " + assembleInput + "(.*)$", "i");
-    var pmCallerRegEx = new RegExp(Util.NedCaller.source + Util.NedPMName.source + ""  + assembleInput + "(.*)$", "i");
-
-    bot.onMessage(callerRegEx, onMessage);
-    bot.onPrivateMessage(pmCallerRegEx, onMessage);
-};
-
-
-var onMessage = function(channel, frm, msg, x) 
-{
-    var self             = this;
-    var isPrivateMessage = (arguments.length == 3) ? true : false;
-    var from             = isPrivateMessage ? '' : frm;
-    var tempMessage      = isPrivateMessage ? frm : msg;
-    var roomName         = channel.split('@')[0];
-    var isSingleWord     = (tempMessage.indexOf(" ") == -1) ? true : false;
-
-    var caller           = /^([\@\!\#\$\~\%\/\\/]*)?/;
-    var ned              = (isPrivateMessage && isSingleWord) ? 
-                           /([Nn][Ee][Dd][Dd]?([Dd]?[Aa][Rr][Dd])?(( *[^A-Za-z0-9]* *))?)?$/ :
-                           /([Nn][Ee][Dd][Dd]?([Dd]?[Aa][Rr][Dd])?(( *[^A-Za-z0-9]* *))? ?)?/; 
-
-    var assembleInput    = "("+trigger.join("|")+")";
-    var regEx            = new RegExp(caller.source + ned.source + assembleInput, "i");
-    var message          = tempMessage.replace(regEx, '');
-    var isEmpty          = (message.split(' ').join('').length == 0 || message.length == 0) ? true : false;
-    var isPrivate        = (isPrivateMessage) ? 1 : 0;
-    var message          = Util.clarify(message.split("+").join("%2B"));
-
-    if(isEmpty)
+    if(get.isEmpty)
     {
-        self.message(channel, Util.errorMessage() + "Please check your input");
+        sendMessage(Util.errorMessage() + "Please check your input");
     }
     else
     {
-        var input = Util.padd(message);
+        var input = Util.padd(get.message);
 
-        // Actual function begins here
-
-
-        if(input.length > 0)
+        var httpRequestParams = 
         {
-            var httpRequestParams = 
-            {
-                host: "gdata.youtube.com",
-                port: 80,
-                path: "/feeds/api/videos?max-results=1&orderBy=relevance&alt=json&q=" + input
-            };
+            host : "gdata.youtube.com",
+            port : 80,
+            path : "/feeds/api/videos?max-results=1&orderBy=relevance&alt=json&q=" + input
+        };
 
-            http.get(httpRequestParams, function(res) 
-            {
-                var data = '';
-                res.on('data', function(chunk) {
-                    data += chunk;
-                });
-                
-                res.on('end', function(chunk) 
-                {
-                    try 
-                    {
-                       res = JSON.parse(data);
-                       vid = res.feed.entry[0].link[0].href
+        http.get(httpRequestParams, function(res) 
+        {
+            var data = '';
 
-                       self.message(channel, vid.split('&feature')[0]);
-                    }
-                    catch(err)
-                    {
-                        self.message(channel, "(facepalm) Error");
-                    }
-                });
+            res.on('data', function(chunk)
+            {
+                data += chunk;
             });
             
-            
-            // actual function ends here
+            res.on('end', function(chunk) 
+            {
+                try 
+                {
+                   var vid = JSON.parse(data).feed.entry[0].link[0].href;
 
-        }
-        else
-        {
-            self.message(channel, Util.errorMessage() + "Please check your input");
-        }
+                   sendMessage(vid.split('&feature')[0]);
+                }
+                catch(err)
+                {
+                    sendMessage("(facepalm) Error");
+                }
+            });
+        });
     }
 
-
     return true;
-};
-
-
+}
