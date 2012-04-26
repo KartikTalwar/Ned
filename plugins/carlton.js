2f9c75e3a17bad729107ffbdfcb6d178bc5bd3cc
KartikTalwar <hi@kartikt.com>
2012-04-25 23:07:29 -0700

    Converted Carlton

--- plugins/carlton.js
+++ plugins/carlton.js
@@ -1,46 +1,29 @@
+var plugin = {
+              name        : 'carlton',
+              trigger     : ".*(dancing|dance|so happy|pumped).*$",
+              enabled     : 'true',
+              fuzzy       : 'false',
+              description : '',
+              usage       : ''
+             };
 
-module.exports.load = function(bot) 
+module.exports.plugin = plugin;
+
+module.exports[plugin.name] = function(get)
 {
-    var assembleInput = /.*(dancing|dance|so happy|so happy|pumped).*/;
-    var callerRegEx   = new RegExp(assembleInput.source, "i");
-    var pmCallerRegEx = new RegExp(assembleInput.source, "i");
+    var resp  = ['http://i47.tinypic.com/2qnytnc.gif', 
+                 'http://i50.tinypic.com/35bib06.jpg'
+                ];
 
-    bot.onMessage(callerRegEx, onMessage);
-    bot.onPrivateMessage(pmCallerRegEx, onMessage);
-};
+    var words = get.message.split(' ').length;
 
-
-var onMessage = function(channel, frm, msg, x) 
-{
-
-    var self             = this;
-    var isPrivateMessage = (arguments.length == 3) ? true : false;
-    var from             = isPrivateMessage ? '' : frm;
-    var tempMessage      = isPrivateMessage ? frm : msg;
-    var roomName         = channel.split('@')[0];
-    var isSingleWord     = (tempMessage.indexOf(" ") == -1) ? true : false;
-    var message          = tempMessage
-
-    var resp  = ['http://media.tumblr.com/tumblr_lrzrlymUZA1qbliwr.gif', 'http://3deadmonkeys.com/gallery3/var/albums/random_stuff/Carlton-Dance-GIF.gif'];
-    
-    var words = message.split(' ').length
-
-    if(Util.triggersRandom([7]) && words > 2)
+    if(Util.triggersRandom([7, 2, 1]) && words > 2)
     {
-        if(message.indexOf('happy') != -1)
-        {
-            var m = "Happy you say..";
-        }
-        else
-        {
-            var m = 'Mee too!!';
-        }
-        
-        self.message(channel, m);
-        self.message(channel, resp[Math.floor(Math.random()*resp.length)]);
+        var msg = 'Mee too!!';
+
+        sendMessage(msg);
+        sendMessage(Util.chooseRandom(resp));
     }
 
-
     return true;
-};
-
+}
