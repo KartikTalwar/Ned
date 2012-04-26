e1d004315dcdf1ee2750c0a53a0d2153a58e7c23
KartikTalwar <hi@kartikt.com>
2012-04-26 11:05:47 -0700

    Converted twss

--- plugins/twss.js
+++ plugins/twss.js
@@ -1,40 +1,23 @@
+var plugin = {
+              name        : 'twss',
+              trigger     : ".* (big|small|long|hard|soft|mouth|face|fast|slow|in there|on there|in that|on that|wet|dry|on the|in the|suck|blow|jaw|all in|fit that|fit it|hurts|hot|huge|balls|stuck)$",
+              enabled     : 'false',
+              fuzzy       : 'false',
+              description : '',
+              usage       : ''
+             };
 
-module.exports.load = function(bot) 
+module.exports.plugin = plugin;
+
+module.exports[plugin.name] = function(get)
 {
-    var assembleInput = /.*(big|small|long|hard|soft|mouth|face|fast|slow|in there|on there|in that|on that|wet|dry|on the|in the|suck|blow|jaw|all in|fit that|fit it|hurts|hot|huge|balls|stuck)$/;
-    var callerRegEx   = new RegExp(assembleInput.source, "i");
-    var pmCallerRegEx = new RegExp(assembleInput.source, "i");
-
-    bot.onMessage(callerRegEx, onMessage);
-    bot.onPrivateMessage(pmCallerRegEx, PM);
-};
-
-
-var onMessage = function(channel, frm, msg, x) 
-{
-
-    var self             = this;
-    var isPrivateMessage = (arguments.length == 3) ? true : false;
-    var from             = isPrivateMessage ? '' : frm;
-    var tempMessage      = isPrivateMessage ? frm : msg;
-    var roomName         = channel.split('@')[0];
-    var isSingleWord     = (tempMessage.indexOf(" ") == -1) ? true : false;
-    var message          = tempMessage
-
     var resp  = "That's what she said!";
-    var words = message.split(' ').length
+    var words = get.message.split(' ').length
 
     if(Util.triggersRandom([3, 7]) && words > 2)
     {
-        self.message(channel, resp);
+        sendMessage(resp);
     }
 
-
-    return true;
-};
-
-var PM = function(channel, frm, msg, x) 
-{
-    this.message(channel, "That's what she said!");
     return true;
 }
