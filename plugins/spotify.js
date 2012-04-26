2c2cfd27165264b22a81562edfdc1a2f8d4c3bf0
KartikTalwar <hi@kartikt.com>
2012-04-25 23:54:52 -0700

    Converted spotify

--- plugins/spotify.js
+++ plugins/spotify.js
@@ -1,102 +1,59 @@
+var plugin = {
+              name        : 'spotify',
+              trigger     : ['spot me', 'spot ', 'spotify', 'sp '],
+              enabled     : 'true',
+              fuzzy       : 'false',
+              description : 'Returns the spotify url of a song',
+              usage       : 'ned spot me everlong'
+             };
 
-var trigger = ['spot me', 'spot ', 'spotify', 'sp '];
-var help    = [{
-               usage       : 'spot me OR spotify me OR sp', 
-               description : 'This will return the requested song [@ned spot me (song)]'
-              }];
-module.exports.help = help
+module.exports.plugin = plugin;
 
-
-
-module.exports.load = function(bot) 
+module.exports[plugin.name] = function(get)
 {
-    var assembleInput = "("+trigger.join("|")+")";    
-    var callerRegEx   = new RegExp(Util.NedCaller.source + Util.NedName.source   + " " + assembleInput + "(.*)$", "i");
-    var pmCallerRegEx = new RegExp(Util.NedCaller.source + Util.NedPMName.source + ""  + assembleInput + "(.*)$", "i");
-
-    bot.onMessage(callerRegEx, onMessage);
-    bot.onPrivateMessage(pmCallerRegEx, onMessage);
-};
-
-
-var onMessage = function(channel, frm, msg, x) 
-{
-    var self             = this;
-    var isPrivateMessage = (arguments.length == 3) ? true : false;
-    var from             = isPrivateMessage ? '' : frm;
-    var tempMessage      = isPrivateMessage ? frm : msg;
-    var roomName         = channel.split('@')[0];
-    var isSingleWord     = (tempMessage.indexOf(" ") == -1) ? true : false;
-
-    var caller           = /^([\@\!\#\$\~\%\/\\/]*)?/;
-    var ned              = (isPrivateMessage && isSingleWord) ? 
-                           /([Nn][Ee][Dd][Dd]?([Dd]?[Aa][Rr][Dd])?(( *[^A-Za-z0-9]* *))?)?$/ :
-                           /([Nn][Ee][Dd][Dd]?([Dd]?[Aa][Rr][Dd])?(( *[^A-Za-z0-9]* *))? ?)?/; 
-
-    var assembleInput    = "("+trigger.join("|")+")";
-    var regEx            = new RegExp(caller.source + ned.source + assembleInput, "i");
-    var message          = tempMessage.replace(regEx, '');
-    var isEmpty          = (message.split(' ').join('').length == 0 || message.length == 0) ? true : false;
-    var isPrivate        = (isPrivateMessage) ? 1 : 0;
-    var message          = Util.clarify(message.split("+").join("%2B"));
-
-    if(isEmpty)
+    if(get.isEmpty)
     {
-        self.message(channel, Util.errorMessage() + "Please check your input");
+        sendMessage(Util.errorMessage() + "Please check your input");
     }
     else
     {
-        var input = Util.padd(message);
+        var input = Util.padd(get.message);
 
-        // Actual function begins here
-
-
-        if(input.length > 0)
+        var httpRequestParams = 
         {
-            var httpRequestParams = 
-            {
-                host: "ws.spotify.com",
-                port: 80,
-                path: "/search/1/track?q=" + input
-            };
+            host : "ws.spotify.com",
+            port : 80,
+            path : "/search/1/track?q=" + input
+        };
 
-            http.get(httpRequestParams, function(res) 
+        http.get(httpRequestParams, function(res) 
+        {
+            var data = '';
+
+            res.on('data', function(chunk) 
             {
-                var data = '';
-                res.on('data', function(chunk) {
-                    data += chunk;
-                });
-                
-                res.on('end', function(chunk) 
-                {
-                    try 
-                    {
-                        id = Util.cut('spotify:track:', '"', data);
-                        if(id.length > 10)
-                        {
-                            response = "http://open.spotify.com/track/" + id;
-                            self.message(channel, response);
-                        }
-                    }
-                    catch(err)
-                    {
-                        self.message(channel, "(facepalm) Error");
-                    }
-                });
+                data += chunk;
             });
             
-            
-            // actual function ends here
+            res.on('end', function(chunk) 
+            {
+                try 
+                {
+                    var id = Util.cut('spotify:track:', '"', data);
 
-        }
-        else
-        {
-            self.message(channel, Util.errorMessage() + "Please check your input");
-        }
+                    if(id.length > 10)
+                    {
+                        response = "http://open.spotify.com/track/" + id;
+                        sendMessage(response);
+                    }
+                }
+                catch(err)
+                {
+                    sendMessage("(facepalm) Error");
+                }
+            });
+        });
     }
 
-
     return true;
-};
-
-
+}
