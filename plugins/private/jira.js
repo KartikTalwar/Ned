f28308923fb36d1ff02ddcc54e73fbcaa0e28d03
Kartik <talwar.kartik@gmail.com>
2012-04-26 19:06:05 -0700

    Updated sample private plugin to new plugin format

--- plugins/private/jira.js
+++ plugins/private/jira.js
@@ -1,71 +1,27 @@
-var trigger = ['jira'];
-var help    = [{
-               usage       : 'jira',
-               description : 'Returns the link to the JIRA bug details [ned jira XY-1234]'
-              }];
-module.exports.help = help
+var plugin = {
+              name        : 'jira',
+              trigger     : ['jira'],
+              enabled     : 'true',
+              fuzzy       : 'false',
+              description : 'Gets JIRA url for a bug',
+              usage       : 'ned JIRA XY-1234'
+             };
 
+module.exports.plugin = plugin;
 
-module.exports.load = function(bot) 
+module.exports[plugin.name] = function(get)
 {
-    var assembleInput = "("+trigger.join("|")+")";    
-    var callerRegEx   = new RegExp(Util.NedCaller.source + Util.NedName.source   + " " + assembleInput + "(.*)$", "i");
-    var pmCallerRegEx = new RegExp(Util.NedCaller.source + Util.NedPMName.source + ""  + assembleInput + "(.*)$", "i");
-
-    bot.onMessage(callerRegEx, onMessage);
-    bot.onPrivateMessage(pmCallerRegEx, onMessage);
-};
-
-
-var onMessage = function(channel, frm, msg, x) 
-{
-    var self             = this;
-    var isPrivateMessage = (arguments.length == 3) ? true : false;
-    var from             = isPrivateMessage ? '' : frm;
-    var tempMessage      = isPrivateMessage ? frm : msg;
-    var roomName         = channel.split('@')[0];
-    var isSingleWord     = (tempMessage.indexOf(" ") == -1) ? true : false;
-
-    var caller           = /^([\@\!\#\$\~\%\/\\/]*)?/;
-    var ned              = (isPrivateMessage && isSingleWord) ? 
-                           /([Nn][Ee][Dd][Dd]?([Dd]?[Aa][Rr][Dd])?(( *[^A-Za-z0-9]* *))?)?$/ :
-                           /([Nn][Ee][Dd][Dd]?([Dd]?[Aa][Rr][Dd])?(( *[^A-Za-z0-9]* *))? ?)?/; 
-
-    var assembleInput    = "("+trigger.join("|")+")";
-    var regEx            = new RegExp(caller.source + ned.source + assembleInput, "i");
-    var message          = tempMessage.replace(regEx, '');
-    var isEmpty          = (message.split(' ').join('').length == 0 || message.length == 0) ? true : false;
-    var isPrivate        = (isPrivateMessage) ? 1 : 0;
-    var message          = Util.clarify(message.split("+").join("%2B"));
-
-    if(isEmpty)
+    if(get.isEmpty)
     {
-        self.message(channel, Util.errorMessage() + "Please check your input");
+        sendMessage(Util.errorMessage() + "Please check your input");
     }
     else
     {
-        var input = Util.padd(message);
+        var input = Util.padd(get.message);
+        var url   = "https://jira.mysite.com/browse/" + input.toUpperCase();
 
-        // Actual function begins here
-
-
-        if(input.length > 0)
-        {
-        
-            url = "https://jira.mysite.com/browse/" + input.toUpperCase();
-
-            self.message(channel, url);
-            
-        // actual function ends here
-
-        }
-        else
-        {
-            self.message(channel, Util.errorMessage() + "Please check your input");
-        }
+        sendMessage(url);
     }
 
-
     return true;
-};
-
+}
